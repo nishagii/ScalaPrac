5ef80d4ff0836a132d21fc6835f86e7a90a7c2a6
nishii <jeewanthadeherath04@gmail.com>
2024-06-16 11:08:29 +0530

    errors solved

--- code1.scala
+++ code1.scala
@@ -1,13 +1,13 @@
 //1 
 def area(r: Double): Double = math.Pi * r * r
 //2
-def far(c:Double):Double= c*1.8*32
+def far(c:Double):Double= c*1.8+32
 //3
-def vol(r:Double):Double=r*math.Pi*4/3
+def vol(r:Double):Double=r*r*r*math.Pi*4/3
 //4
-def disc(x:Double):Double=x*0.6
-def ship(i:Int):Double= if (i<50) {i*3} else {(i-50)*0.75+3*50}
-def tot(i:Int,x:Double):Double=disc(x)+ship(i)
+def disc(i:Double):Double=24.95*0.6*i
+def ship(i:Int):Double= if (i<50) 3 else {(i-50)*0.75+3}
+def tot(i:Int):Double=disc(i)+ship(i)
 //5
 def easy(e:Double):Double=8*e
 def tempo(t:Double):Double=7*t
