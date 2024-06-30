f0dbfe5972b3a36902aebecc3ed242f4e49c2c32
nishii <jeewanthadeherath04@gmail.com>
2024-06-30 19:19:05 +0530

    code4

---
+++ practicle3/code4.scala
@@ -0,0 +1,21 @@
+
+object SumOfEvens {
+  def sumOfEvenNumbers(inputList: List[Int]): Int = {
+    def sumEvens(list: List[Int], acc: Int): Int = {
+      list match {
+        case Nil => acc
+        case head :: tail =>
+          if (head % 2 == 0) sumEvens(tail, acc + head)
+          else sumEvens(tail, acc)
+      }
+    }
+
+    sumEvens(inputList, 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val sum = sumOfEvenNumbers(numbers)
+    println(s"The sum of even numbers in the list is: $sum")
+  }
+}
