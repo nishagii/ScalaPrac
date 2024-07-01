5a8e1fe02a4ba9b69c016afefdb080ea95a27ab5
nishii <jeewanthadeherath04@gmail.com>
2024-07-01 18:52:19 +0530

    ..

--- lecturesCode/tempCodeRunnerFile.scala
+++
@@ -1,8 +0,0 @@
-object blast extends App{
-//     def blast(n:Int):Unit={
-//         if(n>1) blast(n-1)
-//         else println("Blastoff!!")
-//         println(n)
-//     }
-//     blast(3)
-// }
