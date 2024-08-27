3cf6588ac56d07aac46d36759975f50997e1f5fd
nishagii <jeewanthadeherath04@gmail.com>
2024-08-27 19:53:03 +0530

    practicle 7 done

---
+++ practicle7/code2.scala
@@ -0,0 +1,12 @@
+object MathUtils {
+  def calculateSquare(numbers: List[Int]): List[Int] = {
+    numbers.map(num => num * num)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5)
+    val output = MathUtils.calculateSquare(input)
+
+    println(output)
+  }
+}
